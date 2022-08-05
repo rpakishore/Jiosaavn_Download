195ec107c5857b6d32b0e3d16ad43cfc84067df0
rpakishore <rpakishore@gmail.com>
2022-08-04 19:55:17 -0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
-cd C:\Users\rpaki\Documents\GitHub\Jiosaavn_Download
+:: cd C:\Users\rpaki\Documents\GitHub\Jiosaavn_Download
+cd /D "%~dp0"
 @echo Started: %date% %time%
 "venv\Scripts\activate.bat" && python main.py -wc -sd -dp
