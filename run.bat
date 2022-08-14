23fa0292e8e7e536c78676559e1e55e787c14189
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2022-08-14 13:29:56 -0700

    Add run from current directory  with default switches

--- run.bat
+++ run.bat
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 :: cd C:\Users\rpaki\Documents\GitHub\Jiosaavn_Download
 cd /D "%~dp0"
 @echo Started: %date% %time%
+=======
+cd /D "%~dp0"
+>>>>>>> Stashed changes
 "venv\Scripts\activate.bat" && python main.py -wc -sd -dp
